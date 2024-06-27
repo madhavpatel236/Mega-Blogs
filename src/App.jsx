ff49017c748343fdef18d8fe5b181fa97cf926f0
madhavpatel236 <madhavpatel236@gmail.com>
2024-06-27 14:41:26 +0530

    Make a Store and make some components and use them in the main.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,41 @@
-
+import { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import './App.css'
+import authservice from './appwrite/auth'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
 
 function App() {
+
+  const [loading, setloading] = useState(true);
+  const dispatch = useDispatch()
+
+  // Here we with the help of the appwrite services we get the userData and if user is present (userdata available) then we directly login with the help of the useDispatch() the user otherwise we show the logout at every useEffect time (relode Time)
+  useEffect(() => {
+    authservice.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login(userData))
+        } else {
+          dispatch(logout())
+        }
+      })
+      .finally(() => setloading(false))
+
+  }, [])
+
+  return !loading ? (
+    <div className='min-h-sc flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          {/* Outlet */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
+
   return (
     <>
       <h1>Blogs App</h1>
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {Provider} from 'react-redux'
+import store from './store/store.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Provider store={store}> {/* here we provide the store to the app.jsx so that we can use it in the app.jsx */}
     <App />
+    </Provider>
   </React.StrictMode>,
 )
