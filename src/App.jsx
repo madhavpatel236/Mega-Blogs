40b279866e31b102ecd69813ef8d59ec7cabfd75
madhavpatel236 <madhavpatel236@gmail.com>
2024-06-27 16:30:28 +0530

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import { useState, useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import './App.css'
 import authservice from './appwrite/auth'
-import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
+import { Footer, Header } from './components'
+import { login, logout } from './store/authSlice'
 
 function App() {
 
