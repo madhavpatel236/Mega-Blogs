f34696cd369680d0570a60c4c806e64fc85d9aed
madhavpatel236 <madhavpatel236@gmail.com>
2024-07-02 18:32:48 +0530

    add AuthLayout.jsx for improve the security

---
+++ src/components/AuthLayout.jsx
@@ -0,0 +1,36 @@
+// protcted containor
+
+import React, {useEffect, useState} from 'react'
+import {useSelector} from 'react-redux'
+import {useNavigate} from 'react-router-dom'
+
+export default function Protected({children, authentication = true}) {
+
+    const navigate = useNavigate()
+    const [loader, setLoader] = useState(true)
+    const authStatus = useSelector(state => state.auth.status)
+
+    useEffect(() => {
+        //TODO: make it more easy to understand
+
+        // if (authStatus ===true){
+        //     navigate("/")
+        // } else if (authStatus === false) {
+        //     navigate("/login")
+        // }
+        
+        //let authValue = authStatus === true ? true : false
+
+
+        // authstatus & authentication true means you are not authenticated.
+        // in simple term authstatus false means that user will authenticate no need to check further
+        if(authentication && authStatus !== authentication){ // true && false !== true => true
+            navigate("/login")
+        } else if(!authentication && authStatus !== authentication){ // false && false !== true => false
+            navigate("/")
+        }
+        setLoader(false)
+    }, [authStatus, navigate, authentication])
+
+  return loader ? <h1>Loading...</h1> : <>{children}</>
+}
