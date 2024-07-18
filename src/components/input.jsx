6e4db508482011960927b5b2241004e43102c757
madhavpatel236 <madhavpatel236@gmail.com>
2024-07-18 23:43:10 +0530

    changes

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -1,41 +1,27 @@
-// This component generally use for the form inputs, means whenever we need a input field in our application we use this component instand of the make it each time.
+import React, {useId} from 'react'
 
-import React, { useId } from 'react'
-// This is the one type of Syntex which we normally use.
-// function input({}) {
-//     const id = useId();
-//     return (
-//         <div>input</div>
-//     )
-// }
-
-// this is the batter practice to write code in the way which we normally follow.
-
-const Input = React.forwardRef(function Input({
-    type = 'text',
-    lable,
+const Input = React.forwardRef( function Input({
+    label,
+    type = "text",
     className = "",
     ...props
-}, ref) { // here we have a ref which is used to connect the lable.
-    const ID = useId();
+}, ref){
+    const id = useId()
     return (
         <div className='w-full'>
-            {lable &&
-                <lable
-                    className='block mb-1'
-                    htmlFor={ID}>
-                    {lable}
-                </lable>
+            {label && <label 
+            className='inline-block mb-1 pl-1' 
+            htmlFor={id}>
+                {label}
+            </label>
             }
             <input
-                type={type}
-                className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-500 duration-200 border-gray-200 w-full
-                    ${className} `}
-                    ref={ref}
-                    {...props}
-                    id={ID}
+            type={type}
+            className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
+            ref={ref}
+            {...props}
+            id={id}
             />
-
         </div>
     )
 })
